4cd9394615064ca94e3e245b0aa9cebd0343ccc1
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-14 09:45:57 +0900

    삽입정렬 알고리 swap 구현

--- intelliJ-git-exercies/src/week4/day1014/algorithm/insertion_sort/InsertionSort01.java
+++ intelliJ-git-exercies/src/week4/day1014/algorithm/insertion_sort/InsertionSort01.java
@@ -5,6 +5,14 @@ import java.util.Arrays;
 public class InsertionSort01 {
     public void sort(int[] arr){
         int i=1;
+        if(arr[i]<arr[i-1]){
+            swap(arr, i, i-1);
+        }
+    }
+    public void swap(int[] arr, int i, int j){
+        int tmp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
     }
     public static void main(String[] args) {
         int[] arr={8, 5, 6, 2, 4};
