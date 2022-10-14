69c4ba5bd0506bce11830971e324cb37fc5ccf55
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-14 09:39:26 +0900

    삽입정렬 알고리즘

---
+++ intelliJ-git-exercies/src/week4/day1014/algorithm/insertion_sort/InsertionSort01.java
@@ -0,0 +1,15 @@
+package week4.day1014.algorithm.insertion_sort;
+
+import java.util.Arrays;
+
+public class InsertionSort01 {
+    public void sort(int[] arr){
+        int i=1;
+    }
+    public static void main(String[] args) {
+        int[] arr={8, 5, 6, 2, 4};
+        InsertionSort01 insertionSort = new InsertionSort01();
+        insertionSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
