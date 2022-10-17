291dc9e75564057cc50fbf9a288b2ecbf226ba9a
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-17 09:06:45 +0900

    직각삼각형 별찍기 4단

---
+++ intelliJ-git-exercies/src/week5/day1017/algorithm/RightTriangle.java
@@ -0,0 +1,10 @@
+package week5.day1017.algorithm;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
