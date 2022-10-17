18be5f97a6f5d434a830ce634af487ac1d47a0fa
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-17 12:37:49 +0900

    마름모 별찍기

---
+++ intelliJ-git-exercies/src/week5/day1017/algorithm/Diamond.java
@@ -0,0 +1,41 @@
+package week5.day1017.algorithm;
+
+import java.util.Scanner;
+
+public class Diamond {
+    private char c;
+    public Diamond(){
+        this.c='*';
+    }
+    public Diamond(char c){
+        this.c=c;
+    }
+    public void printDiamod(int n){
+        for (int i = 0; i < n; i++) {
+            for(int j=n-1-i; j>0; j--){
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*i+1; j++) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<(n-i-1)*2-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        char c=sc.next().charAt(0);
+        Diamond diamond=new Diamond(c);
+        diamond.printDiamod(n);
+    }
+}
