908bf12ba243aabea98b81e4ed25512f6ced3b37
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-17 13:17:43 +0900

    DB Select

---
+++ hospital/src/main/java/com/dbexercise/User.java
@@ -0,0 +1,29 @@
+package com.dbexercise;
+
+public class User {
+    private String id;
+    private String name;
+    private String password;
+
+    public User(String id, String name, String password){
+        this.id=id;
+        this.name=name;
+        this.password=password;
+    }
+
+    public String getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getPassword(){
+        return password;
+    }
+
+    public void printUserInfo(){
+        System.out.println("id : " + id);
+        System.out.println("name : "+name);
+        System.out.println("password : "+password);
+    }
+}
--- hospital/src/main/java/com/dbexercise/UserDao.java
+++ hospital/src/main/java/com/dbexercise/UserDao.java
@@ -1,10 +1,9 @@
 package com.dbexercise;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class UserDao {
@@ -25,9 +24,34 @@ public class UserDao {
         ps.close();
         conn.close();
     }
+    public List<User> searchId(String id) throws ClassNotFoundException, SQLException {
+        Map<String, String> env = System.getenv();
+        String dbHost=env.get("DB_HOST");
+        String dbUser=env.get("DB_USER");
+        String dbPassword=env.get("DB_PASSWORD");
 
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword); //db연결
+
+        String query="Select * from users where id='"+id+"';";
+        Statement stmt = conn.createStatement();
+        ResultSet result=stmt.executeQuery(query);
+
+        List<User> users = new ArrayList<>();
+        while(result.next()){
+            users.add(new User(result.getString("id"), result.getString("name"), result.getString("password")));
+        }
+
+        conn.close();
+        return users;
+    }
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
-        userDao.add();
+        //userDao.add();
+        List<User> users=new ArrayList<>();
+        users=userDao.searchId("0");
+        for(User user : users){
+            user.printUserInfo();
+        }
     }
 }
