5ead61dad20f5a974df7d4ce927b8a6fc7224c04
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-18 09:31:05 +0900

    codeup 1859 재귀로 별찍기

---
+++ intelliJ-git-exercies/src/week5/day1018/algorithm/codeup/codeup_1859/Main.java
@@ -0,0 +1,25 @@
+package week5.day1018.algorithm.codeup.codeup_1859;
+
+import java.util.Scanner;
+
+public class Main {
+    void printARow(int n){
+        if(n==0) return;
+        printARow(n-1);
+        System.out.print("*");
+    }
+    void printACol(int n){
+        if(n==0) return;
+
+        printACol(n-1);
+        printARow(n);
+
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        Main m=new Main();
+        m.printACol(n);
+    }
+}
