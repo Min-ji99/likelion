db49e70e30a6ad6f1bad7592f70a07c0780be803
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-18 09:22:09 +0900

    직사각형 별찍기

---
+++ intelliJ-git-exercies/src/week5/day1018/algorithm/RectangleStar.java
@@ -0,0 +1,31 @@
+package week5.day1018.algorithm;
+
+import java.util.Scanner;
+
+public class RectangleStar {
+    private char c;
+    public RectangleStar(){
+        this.c='*';
+    }
+    public RectangleStar(char c){
+        this.c=c;
+    }
+    void printSquareMatrix(int n, int m){
+        for(int i=0; i<n; i++){
+            /*
+            for(int j=0; j<n; j++){
+                System.out.print(c);
+            }
+            System.out.println();
+             */
+            System.out.println("*".repeat(m));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        RectangleStar rectangleStar = new RectangleStar();
+        rectangleStar.printSquareMatrix(n, m);
+    }
+}
