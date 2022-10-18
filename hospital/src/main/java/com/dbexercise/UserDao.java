ab1f412584be55690ada9cb61b56a341b4027902
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-18 11:35:17 +0900

    UserDaoTest

--- hospital/src/main/java/com/dbexercise/UserDao.java
+++ hospital/src/main/java/com/dbexercise/UserDao.java
@@ -4,53 +4,92 @@ package com.dbexercise;
 import com.dbexercise.domain.User;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class UserDao {
-    public void add() throws SQLException, ClassNotFoundException {
+    public void add(User user) {
         Map<String, String> env = System.getenv();
-        String dbHost=env.get("DB_HOST");
-        String dbUser=env.get("DB_USER");
-        String dbPassword=env.get("DB_PASSWORD");
+        try {
+            String dbHost = env.get("DB_HOST");
+            String dbUser = env.get("DB_USER");
+            String dbPassword = env.get("DB_PASSWORD");
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword); //db연결
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES(?, ?, ?)");
-        ps.setString(1, "1");
-        ps.setString(2, "Glidong");
-        ps.setString(3, "0101");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword); //db연결
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES(?, ?, ?)");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
-        int status = ps.executeUpdate();
-        ps.close();
-        conn.close();
-    }
-    public User searchId(String id) throws ClassNotFoundException, SQLException {
-        Map<String, String> env = System.getenv();
-        String dbHost=env.get("DB_HOST");
-        String dbUser=env.get("DB_USER");
-        String dbPassword=env.get("DB_PASSWORD");
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword); //db연결
-
-        String query="Select * from users where id='"+id+"';";
-        Statement stmt = conn.createStatement();
-        ResultSet result=stmt.executeQuery(query);
-
-        User user=new User();
-        while(result.next()){
-            user.setId(result.getString("id"));
-            user.setName(result.getString("name"));
-            user.setPassword(result.getString("password"));
+            int status = ps.executeUpdate();
+            ps.close();
+            conn.close();
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
         }
 
-        conn.close();
-        return user;
+    }
+    public User searchId(String id) {
+        Map<String, String> env = System.getenv();
+        try {
+            String dbHost = env.get("DB_HOST");
+            String dbUser = env.get("DB_USER");
+            String dbPassword = env.get("DB_PASSWORD");
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword); //db연결
+
+            String query = "Select * from users where id='" + id + "';";
+            Statement stmt = conn.createStatement();
+            ResultSet result = stmt.executeQuery(query);
+            result.next();
+            User user = new User(result.getString("id"), result.getString("name"), result.getString("password"));
+
+            conn.close();
+
+            return user;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public List<User> findAll() throws SQLException, ClassNotFoundException {
+        Map<String, String> env = System.getenv();
+        String dbHost=env.get("DB_HOST");
+        String dbUser=env.get("DB_USER");
+        String dbPassword=env.get("DB_PASSWORD");
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword); //db연결
+
+        List<User> users=new ArrayList<>();
+
+        Statement stmt=conn.createStatement();
+        ResultSet result=stmt.executeQuery("Select * from users;");
+
+        while(result.next()){
+            users.add(new User(result.getString("id"), result.getString("name"), result.getString("password")));
+        }
+
+        return users;
     }
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
-        //userDao.add();
+        userDao.add(new User("2", "Ruru", "1234qwer"));
+        /*
         User user=userDao.searchId("0");
         user.printUserInfo();
+        */
+
+        List<User> users=new ArrayList<>();
+        for(User user : users){
+            user.printUserInfo();
+        }
     }
 }
