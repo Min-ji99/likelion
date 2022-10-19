dd6410de9cdc2f41cd1fdd7f3611ce71a7fb6b67
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-10-19 13:41:00 +0900

    spring 적용

--- dbconnection/src/main/java/com.line/dao/UserDaoFactory.java
+++ dbconnection/src/main/java/com.line/dao/UserDaoFactory.java
@@ -1,7 +1,12 @@
 package com.line.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
     //조립
+    @Bean
     public UserDao awsUserDao(){
         UserDao userDao=new UserDao(new AwsConnectionMaker());
         return userDao;
--- dbconnection/src/test/java/line/dao/UserDaoTest.java
+++ dbconnection/src/test/java/line/dao/UserDaoTest.java
@@ -6,14 +6,24 @@ import com.line.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 
 class UserDaoTest {
+    @Autowired
+    ApplicationContext context;
+
     @Test
     @DisplayName("users table insert 확인")
     void addAndSelect(){
         UserDao userDao = new UserDaoFactory().awsUserDao();
-        User user = new User("9", "nana", "2022");
+        User user = new User("10", "nana", "2022");
         userDao.add(user);
 
         User selectedUser = userDao.searchId("8");
