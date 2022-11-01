0c51f5b2283a1853f318b88c7b2dbfed25ae2afb
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-01 10:33:17 +0900

    소수찾기 템플릿콜백 적용

--- intelliJ-git-exercies/src/main/java/week7/pg_12921/TemplateCallbackPrime.java
+++ intelliJ-git-exercies/src/main/java/week7/pg_12921/TemplateCallbackPrime.java
@@ -3,11 +3,7 @@ package week7.pg_12921;
 interface StatementStrategy{
     boolean compare(int a, int b);
 }
-public class TemplateCallbackPrime implements  StatementStrategy{
-    @Override
-    public boolean compare(int a, int b) {
-        return a<b;
-    }
+public class TemplateCallbackPrime{
     boolean isPrime(int num, StatementStrategy stmt){
         for(int i=2; stmt.compare(i, num); i++){
             if(num%i==0) return false;
@@ -17,7 +13,12 @@ public class TemplateCallbackPrime implements  StatementStrategy{
 
     public static void main(String[] args) {
         TemplateCallbackPrime templateCallbackPrime=new TemplateCallbackPrime();
-        System.out.println(templateCallbackPrime.isPrime(13));
+        System.out.println(templateCallbackPrime.isPrime(13, new StatementStrategy() {
+            @Override
+            public boolean compare(int a, int b) {
+                return a<b;
+            }
+        }));
     }
 
 
