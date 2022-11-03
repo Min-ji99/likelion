a00501dd4f8c63d1a0d1668dbb9911deb5ba9c5f
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-03 10:17:27 +0900

    에라토스테네스의 체 2를 제외한 2의 배수 제외

--- intelliJ-git-exercies/src/main/java/week7/pg_12921/RemoveMultiOf.java
+++ intelliJ-git-exercies/src/main/java/week7/pg_12921/RemoveMultiOf.java
@@ -5,27 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RemoveMultiOf {
-    public int solution(int num){
-        //list 이용한 에라토스테네스의 체
-        List<Integer> numbers=new ArrayList<>();
-        //2부터 num까지 숫자가 들어있는 List 만들기
-        for(int i=2; i<=num; i++){
-            numbers.add(i);
-        }
-        //리스트에서 자기 자신을 제외한 모든 배수 지우기(num의 제곱근까지)
-        for(int i=2; i*i<=num; i++) {
-            for (int j =2; j * i <= num; j++) {
-                if (numbers.indexOf(i * j) > -1) {
-                    numbers.remove(numbers.indexOf(i * j));
-                }
-            }
-        }
-        for(Integer number:numbers){
-            System.out.println(number);
-        }
-        //소수 개수 출력
-        return numbers.size();
-    }
     public static void main(String[] args){
         int n=50;
         int[] nums=new int[n-1];
