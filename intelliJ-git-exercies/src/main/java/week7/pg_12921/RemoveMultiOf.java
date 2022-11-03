b42cdfc3502fecbdbff80cb310579c87a7094ccf
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-03 10:16:42 +0900

    에라토스테네스의 체 2를 제외한 2의 배수 제외

--- intelliJ-git-exercies/src/main/java/week7/pg_12921/RemoveMultiOf.java
+++ intelliJ-git-exercies/src/main/java/week7/pg_12921/RemoveMultiOf.java
@@ -6,14 +6,6 @@ import java.util.List;
 
 public class RemoveMultiOf {
     public int solution(int num){
-        int n=50;
-        int[] nums=new int[n-1];
-        boolean[] checks=new boolean[nums.length];
-
-        Arrays.fill(checks, true);
-
-        for(int i=0; i<nums.length; i++) nums[i]=i+2;
-        /*
         //list 이용한 에라토스테네스의 체
         List<Integer> numbers=new ArrayList<>();
         //2부터 num까지 숫자가 들어있는 List 만들기
@@ -33,6 +25,24 @@ public class RemoveMultiOf {
         }
         //소수 개수 출력
         return numbers.size();
-         */
+    }
+    public static void main(String[] args){
+        int n=50;
+        int[] nums=new int[n-1];
+        boolean[] checks=new boolean[nums.length];
+
+        Arrays.fill(checks, true);
+
+        for(int i=0; i<nums.length; i++) nums[i]=i+2;
+
+        int multipleOf=2;
+        for(int i=multipleOf*2; i<nums.length; i+=2){
+            checks[i]=false;
+        }
+
+        for(int i=0; i<nums.length; i++){
+            if(checks[i]) System.out.println(nums[i]);
+        }
+
     }
 }
