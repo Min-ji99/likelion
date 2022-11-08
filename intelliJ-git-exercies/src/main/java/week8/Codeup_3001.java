19b855e2792bc03fece1c0c2030cfd8bfcdb156e
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-08 09:14:49 +0900

    코드업 3001 해결 완료

---
+++ intelliJ-git-exercies/src/main/java/week8/Codeup_3001.java
@@ -0,0 +1,26 @@
+package week8;
+
+import java.util.Scanner;
+
+public class Codeup_3001 {
+    public static int findUseLoop(int value, int n, int[] arr){
+        int idx=-1;
+        for(int i=0; i<n; i++){
+            if(value==arr[i]) idx=i;
+        }
+        if(idx!=-1) idx+=1;
+        return idx;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Codeup_3001 codeup=new Codeup_3001();
+
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        int value=sc.nextInt();
+        System.out.println(findUseLoop(value, n, arr));
+    }
+}
