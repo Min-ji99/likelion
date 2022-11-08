4a671023a2461ae558c7dab1eb06a8b01fa71bbd
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-08 10:24:07 +0900

    코드업 2083 해결 완료

---
+++ intelliJ-git-exercies/src/main/java/week8/codeup_2083/Codeup_2083.java
@@ -0,0 +1,33 @@
+package week8.codeup_2083;
+
+import java.util.Scanner;
+
+public class Codeup_2083 {
+    public int binarySearch(int[] arr, int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==target){
+                return mid+1;
+            }
+            if(arr[mid]>target){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Codeup_2083 codeup=new Codeup_2083();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int target=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(codeup.binarySearch(arr, target));
+    }
+}
