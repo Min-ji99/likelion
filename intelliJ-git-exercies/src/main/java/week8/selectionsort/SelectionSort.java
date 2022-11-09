2f523f9c086a5fd820d63d17087eac778c946fc0
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-09 09:31:04 +0900

    선택정렬 구현 완료

--- intelliJ-git-exercies/src/main/java/week8/selectionsort/SelectionSort.java
+++ intelliJ-git-exercies/src/main/java/week8/selectionsort/SelectionSort.java
@@ -1,12 +1,27 @@
 package week8.selectionsort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+    public void sorting(int[] arr){
+        for(int i=0; i<arr.length-1; i++) {
+            int idx=i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[idx] > arr[j]) idx = j;
+            }
+            swap(arr, idx, i);
+        }
+    }
+    public void swap(int[] arr, int i, int j){
+        int tmp=arr[j];
+        arr[j]=arr[i];
+        arr[i]=tmp;
+    }
     public static void main(String[] args) {
         int[] arr=new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+        SelectionSort selectionSort=new SelectionSort();
+        selectionSort.sorting(arr);
 
-        int idx=0;
-        for(int i=1; i<arr.length; i++){
-            if(arr[idx]>arr[i]) idx=i;
-        }
+        System.out.println(Arrays.toString(arr));
     }
 }
