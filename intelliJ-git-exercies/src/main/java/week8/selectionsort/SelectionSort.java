975169723c5543a3c569b27bc3dd058a6a333119
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-09 10:05:14 +0900

    선택정렬 내림차순 추가

--- intelliJ-git-exercies/src/main/java/week8/selectionsort/SelectionSort.java
+++ intelliJ-git-exercies/src/main/java/week8/selectionsort/SelectionSort.java
@@ -2,12 +2,15 @@ package week8.selectionsort;
 
 import java.util.Arrays;
 
+interface StatementStrategy{
+    boolean compare(int a, int b);
+}
 public class SelectionSort {
-    public void sorting(int[] arr){
+    public void sorting(int[] arr, StatementStrategy stmt){
         for(int i=0; i<arr.length-1; i++) {
             int idx=i;
             for (int j = i+1; j < arr.length; j++) {
-                if (arr[idx] > arr[j]) idx = j;
+                if (stmt.compare(arr[idx], arr[j])) idx = j;
             }
             swap(arr, idx, i);
         }
@@ -20,8 +23,22 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr=new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
         SelectionSort selectionSort=new SelectionSort();
-        selectionSort.sorting(arr);
+        //내림차순 정렬
+        selectionSort.sorting(arr, new StatementStrategy(){
+            @Override
+            public boolean compare(int idx, int a) {
+                return idx<a;
+            }
+        } );
+        System.out.println(Arrays.toString(arr));
 
+        //오름차순 정렬
+        selectionSort.sorting(arr, new StatementStrategy(){
+            @Override
+            public boolean compare(int idx, int a) {
+                return idx>a;
+            }
+        } );
         System.out.println(Arrays.toString(arr));
     }
 }
