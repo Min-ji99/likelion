461b654a4e3e6833930e4283db87be44ebaf6a3d
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-10 09:28:18 +0900

    Comment : 스택사용한 경우와 스택을 사용하지 않은 경우 효율성 결과 추가

--- intelliJ-git-exercies/src/main/java/week8/pg_12906/PG_12906.java
+++ intelliJ-git-exercies/src/main/java/week8/pg_12906/PG_12906.java
@@ -2,7 +2,20 @@ package week8.pg_12906;
 
 import java.util.ArrayList;
 import java.util.Stack;
+/*
+효율성
+solution
+테스트 1 〉	통과 (22.09ms, 115MB)
+테스트 2 〉	통과 (23.42ms, 115MB)
+테스트 3 〉	통과 (23.45ms, 115MB)
+테스트 4 〉	통과 (23.95ms, 111MB)
 
+solution2
+테스트 1 〉	통과 (37.94ms, 116MB)
+테스트 2 〉	통과 (37.51ms, 111MB)
+테스트 3 〉	통과 (40.80ms, 113MB)
+테스트 4 〉	통과 (39.20ms, 111MB)
+ */
 public class PG_12906 {
     //스택 안쓰고
     public int[] solution(int[] arr) {
