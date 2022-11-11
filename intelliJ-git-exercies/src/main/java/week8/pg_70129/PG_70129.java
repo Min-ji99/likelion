7a2b079a2aa03956b1eec5a461a37e0f769801a3
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-11 10:06:30 +0900

    Comment : 프로그래머스 70129 주석 추가

--- intelliJ-git-exercies/src/main/java/week8/pg_70129/PG_70129.java
+++ intelliJ-git-exercies/src/main/java/week8/pg_70129/PG_70129.java
@@ -7,8 +7,8 @@ public class PG_70129 {
         int zeroCnt=0;
         while(!s.equals("1")){
             zeroCnt+=s.length()-s.replace("0", "").length();
-            s=s.replace("0", "");
-            s=Integer.toBinaryString(s.length());
+            s=s.replace("0", ""); //0제거
+            s=Integer.toBinaryString(s.length()); //문자열 길이를 2진수로 변환
             cnt+=1;
         }
         answer[0]=cnt;
