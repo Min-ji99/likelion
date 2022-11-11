a96fa5a6cb04c465ed4312f27a81b8f575efbf5f
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-11 09:08:01 +0900

    Comment : 프로그래머스 17681 구현 방법 떠올리기

---
+++ intelliJ-git-exercies/src/main/java/week8/pg_17681/PG_17681.java
@@ -0,0 +1,15 @@
+package week8.pg_17681;
+/*
+1. 전체 지도 구하기
+   비트 연산자 or로 계산
+2. 연산 결과를 이진수로 변경
+    1이면 # 아니면 공백
+ */
+public class PG_17681 {
+    class Solution {
+        public String[] solution(int n, int[] arr1, int[] arr2) {
+            String[] answer = {};
+            return answer;
+        }
+    }
+}
