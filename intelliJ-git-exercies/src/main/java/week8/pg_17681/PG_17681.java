4b6d2c55f5bd773975dd5e033bc5f8f789733fa0
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-11 09:15:58 +0900

    Fix : 전체 지도 구하기

--- intelliJ-git-exercies/src/main/java/week8/pg_17681/PG_17681.java
+++ intelliJ-git-exercies/src/main/java/week8/pg_17681/PG_17681.java
@@ -1,4 +1,7 @@
 package week8.pg_17681;
+
+import java.util.Arrays;
+
 /*
 1. 전체 지도 구하기
    비트 연산자 or로 계산
@@ -6,10 +9,14 @@ package week8.pg_17681;
     1이면 # 아니면 공백
  */
 public class PG_17681 {
-    class Solution {
         public String[] solution(int n, int[] arr1, int[] arr2) {
             String[] answer = {};
+            int[] maps=new int[arr1.length];
+
+            for(int i=0; i<arr1.length; i++){
+                maps[i]=arr1[i]|arr2[i];
+            }
+
             return answer;
         }
-    }
 }
---
+++ intelliJ-git-exercies/src/test/java/week8/pg_17681/PG_17681Test.java
@@ -0,0 +1,28 @@
+package week8.pg_17681;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PG_17681Test {
+    PG_17681 pg_17681;
+    int n;
+    int[] arr1;
+    int[] arr2;
+    String[] result;
+
+    @BeforeEach
+    void setup(){
+        pg_17681=new PG_17681();
+        n=5;
+        arr1=new int[]{9, 20, 28, 18, 11};
+        arr2=new int[]{30, 1, 21, 17, 28};
+        result=new String[]{"#####","# # #", "### #", "# ##", "#####"};
+    }
+
+    @Test
+    void solutionTest(){
+        assertArrayEquals(result, pg_17681.solution(n, arr1, arr2));
+    }
+}
