70e670fb13738adc424b40867486040a68b1d792
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-15 10:16:40 +0900

    Add : 퀵 정렬 구현 완료

--- intelliJ-git-exercies/src/main/java/week9/day1115/QuickSort.java
+++ intelliJ-git-exercies/src/main/java/week9/day1115/QuickSort.java
@@ -1,21 +1,36 @@
 package week9.day1115;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class QuickSort {
-    public static void main(String[] args) {
-        int[] arr=new int[]{20, 18, 5, 19, 5, 25, 40, 50};
-        int pivot=arr[arr.length/2];
+    public List<Integer> sorting(List<Integer> arr){
+        if(arr.size()<=1) return arr;
+        Integer pivot=arr.get(arr.size()/2);
         List<Integer> left=new ArrayList<>();
         List<Integer> right=new ArrayList<>();
+        List<Integer> equalPivot=new ArrayList<>();
 
-        for(int num: arr){
-            if(num<=pivot){
-                left.add(num);
-            }else{
-                right.add(num);
-            }
+        for(int i=0; i<arr.size(); i++){
+            if(arr.get(i)<pivot) left.add(arr.get(i));
+            else if(arr.get(i)>pivot) right.add(arr.get(i));
+            else equalPivot.add(arr.get(i));
         }
+
+        List<Integer> mergeList=new ArrayList<>();
+        mergeList.addAll(sorting(left));
+        mergeList.addAll(equalPivot);
+        mergeList.addAll(sorting(right));
+
+        return mergeList;
+    }
+    public static void main(String[] args) {
+        QuickSort quickSort=new QuickSort();
+        Integer[] arr=new Integer[]{20, 18, 5, 19, 5, 25, 40, 50};
+        System.out.println(quickSort.sorting(Arrays.asList(arr)));
     }
 }
