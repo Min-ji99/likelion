847646e52884744b73bfe1e3d61266eeef2073e1
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-17 09:44:14 +0900

    Add : 퀵 정렬 교환 로직 반복 구현

---
+++ intelliJ-git-exercies/src/main/java/week9/day1117/QuickSort.java
@@ -0,0 +1,37 @@
+package week9.day1117;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public int[] sort(int[] arr, int startIdx, int endIdx) {
+
+        int pivot = arr[arr.length / 2];
+        System.out.println(pivot);
+        int leftIdx = 0;
+        int rightIdx = arr.length - 1;
+
+        while (true) {
+            while (arr[leftIdx] < pivot) leftIdx += 1;
+            while (arr[rightIdx] > pivot) rightIdx -= 1;
+            if(leftIdx>rightIdx) break;
+            swap(arr, leftIdx, rightIdx);
+            leftIdx += 1;
+            rightIdx -= 1;
+
+            System.out.printf("4:%d 7:%d\n", arr[4], arr[7]); // 25, 40
+            System.out.printf("leftIdx:%d rightIdx:%d\n", leftIdx, rightIdx); // 5, 6
+            System.out.println(Arrays.toString(arr));
+        }
+        return arr;
+    }
+    public void swap(int[] arr, int x, int y){
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+    public static void main(String[] args) {
+        var arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+        QuickSort quickSort=new QuickSort();
+        quickSort.sort(arr, 0, arr.length-1);
+    }
+}
