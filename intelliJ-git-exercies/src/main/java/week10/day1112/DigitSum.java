c770514f21226f4d000943511b8fd6666dbd107e
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-22 10:25:54 +0900

    Add : 재귀호출로 각 자리수의 합 구하기

---
+++ intelliJ-git-exercies/src/main/java/week10/day1112/DigitSum.java
@@ -0,0 +1,16 @@
+package week10.day1112;
+
+import java.util.Scanner;
+
+public class DigitSum {
+    public long sum(long n){
+        if(n==0) return 0;
+        return n%10+sum(n/10);
+    }
+    public static void main(String[] args) {
+        DigitSum digitSum=new DigitSum();
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        System.out.println(digitSum.sum(n));
+    }
+}
