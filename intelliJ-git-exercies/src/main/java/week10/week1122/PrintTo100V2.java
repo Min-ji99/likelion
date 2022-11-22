f496da259a3e8f3cc073af04c73bde7e39ef8ff6
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-22 09:17:21 +0900

    Add : 재귀호출 탈출 조건 넣기

--- intelliJ-git-exercies/src/main/java/week10/week1122/PrintTo100V2.java
+++ intelliJ-git-exercies/src/main/java/week10/week1122/PrintTo100V2.java
@@ -2,6 +2,7 @@ package week10.week1122;
 
 public class PrintTo100V2 {
     public static void print1To100(int n) {
+        if(n==100) return;
         System.out.println(n);
         print1To100(n);
     }
