3ba1155b3767e61af5d4233b83147e41c9f58c4f
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-24 09:39:13 +0900

    Feat : codeup 1857 해결

---
+++ intelliJ-git-exercies/src/main/java/week10/day1124/codeup_1857.java
@@ -0,0 +1,18 @@
+package week10.day1124;
+
+import java.util.Scanner;
+
+public class codeup_1857 {
+    public static int solution(int n, int r){
+        if(r>n) return 0;
+        if(r==0 || r==n) return 1;
+        if(r==1) return n;
+        return solution(n-1, r-1)+solution(n-1, r);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int r=sc.nextInt();
+        System.out.println(solution(n, r));
+    }
+}
