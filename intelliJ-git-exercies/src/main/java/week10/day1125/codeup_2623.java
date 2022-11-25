1fcdb58542556c9f9eed547b27dd7844b19ec67e
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-25 10:06:45 +0900

    Feat : (반복문)codeup 2623 최대공약수 해결

--- intelliJ-git-exercies/src/main/java/week10/day1125/codeup_2623.java
+++ intelliJ-git-exercies/src/main/java/week10/day1125/codeup_2623.java
@@ -3,16 +3,29 @@ package week10.day1125;
 import java.util.Scanner;
 
 public class codeup_2623 {
-    public static int solution(int a, int b){
+    public static int recursiveSolution(int a, int b){
         if(a==b) return a;
-        if(a>b) return solution(a-b, b);
-        return solution(a, b-a);
+        if(a>b) return recursiveSolution(a-b, b);
+        return recursiveSolution(a, b-a);
     }
-
+    public static int loopSolution(int a, int b){
+        if(b>a){
+            int tmp=a;
+            a=b;
+            b=tmp;
+        }
+        while(b!=0){
+            int n=a%b;
+            a=b;
+            b=n;
+        }
+        return a;
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
         int b=sc.nextInt();
-        System.out.println(solution(a, b));
+        System.out.println(recursiveSolution(a, b));
+        System.out.println(loopSolution(a, b));
     }
 }
