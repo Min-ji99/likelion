10b992c5812adc51b50b0016bf6fb7d009dc1a33
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-28 09:36:14 +0900

    Feat : 알파벳 한개 출력(재귀)

--- intelliJ-git-exercies/src/main/java/week11/day1128/AlphabetCombination.java
+++ intelliJ-git-exercies/src/main/java/week11/day1128/AlphabetCombination.java
@@ -1,12 +1,12 @@
 package week11.day1128;
 
 public class AlphabetCombination {
-    private static final String alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static void printCombination(char c){
+        if(c>'Z') return;
+        System.out.println(c);
+        printCombination((char)(c+1));
+    }
     public static void main(String[] args) {
-        for(int i=0; i<alphabet.length(); i++){
-            for(int j=0; j<alphabet.length(); j++) {
-                System.out.println(String.valueOf(alphabet.charAt(i)) + String.valueOf(alphabet.charAt(j)));
-            }
-        }
+        printCombination('A');
     }
 }
