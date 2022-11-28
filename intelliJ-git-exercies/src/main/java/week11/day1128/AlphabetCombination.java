4f2b59954adf7bcec09a2155016f54da96bbf556
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-28 10:17:05 +0900

    Feat : 알파벳 두개 출력(재귀)

--- intelliJ-git-exercies/src/main/java/week11/day1128/AlphabetCombination.java
+++ intelliJ-git-exercies/src/main/java/week11/day1128/AlphabetCombination.java
@@ -1,12 +1,14 @@
 package week11.day1128;
 
 public class AlphabetCombination {
-    public static void printCombination(char c){
-        if(c>'Z') return;
-        System.out.println(c);
-        printCombination((char)(c+1));
+    public static void printCombination(String prefix){
+        if(prefix.length()>2) return;
+        System.out.println(prefix);
+        for(char c='A'; c<='Z'; c++){
+            printCombination(prefix+c);
+        }
     }
     public static void main(String[] args) {
-        printCombination('A');
+        printCombination("");
     }
 }
