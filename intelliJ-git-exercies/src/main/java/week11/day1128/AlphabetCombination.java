75ecb2678e9f60bf9235dc91557d2a49942515e0
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-28 09:18:52 +0900

    Feat : 알파벳 한개 조합

---
+++ intelliJ-git-exercies/src/main/java/week11/day1128/AlphabetCombination.java
@@ -0,0 +1,10 @@
+package week11.day1128;
+
+public class AlphabetCombination {
+    private static final String alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static void main(String[] args) {
+        for(int i=0; i<alphabet.length(); i++){
+            System.out.println(alphabet.charAt(i));
+        }
+    }
+}
