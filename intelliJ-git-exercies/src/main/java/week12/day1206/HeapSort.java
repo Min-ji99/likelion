e875e94bcb2a36bff6a498ff073e61b2827e50bd
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-06 10:31:27 +0900

    Feat : 힙 정렬 구현

--- intelliJ-git-exercies/src/main/java/week12/day1206/HeapSort.java
+++ intelliJ-git-exercies/src/main/java/week12/day1206/HeapSort.java
@@ -3,19 +3,19 @@ package week12.day1206;
 import java.util.Arrays;
 
 public class HeapSort {
-    public static int[] makeHeap(int[] arr, int idx){
+    public static int[] makeHeap(int[] arr, int idx, int arrSize){
         int left=idx*2+1;
         int right=idx*2+2;
-        int parent=idx;
-        if(left<arr.length && arr[parent]<arr[left]){
-            parent=left;
+        int great=idx;
+        if(left<arrSize && arr[great]<arr[left]){
+            great=left;
         }
-        if(right<arr.length && arr[parent]<arr[right]){
-            parent=right;
+        if(right<arrSize && arr[great]<arr[right]){
+            great=right;
         }
-        if(idx!=parent){
-            swap(arr, parent, idx);
-            makeHeap(arr, parent);
+        if(idx!=great){
+            swap(arr, great, idx);
+            makeHeap(arr, great, arrSize);
         }
         return arr;
     }
@@ -27,9 +27,14 @@ public class HeapSort {
     public static void main(String[] args) {
         int[] arr = new int[]{6, 5, 7, 8};
         arr = new int[]{5, 8, 4, 7, 3, 2, 9, 6, 7};
-        for (int j = (arr.length-2)/2; j >= 0 ; j--) {
-            arr = makeHeap(arr, j);
+        for (int j = arr.length/2-1; j >= 0 ; j--) {
+            arr = makeHeap(arr, j, arr.length);
             System.out.println(Arrays.toString(arr));
         }
+        for(int i=arr.length-1; i>=0; i--){
+            swap(arr, 0, i);
+            arr=makeHeap(arr, 0, i);
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
