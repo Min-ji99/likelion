c8f81605e386dfc6919e6316e89bd2607173d3ab
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-08 10:05:05 +0900

    Feat : 피보나치 재귀 없이 dp로 구현

--- intelliJ-git-exercies/src/main/java/week12/day1208/Fibonacci.java
+++ intelliJ-git-exercies/src/main/java/week12/day1208/Fibonacci.java
@@ -12,6 +12,11 @@ public class Fibonacci {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
         int[] dp=new int[n+1];
-        System.out.println(fibo(n, dp));
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.println(dp[n]);
     }
 }
