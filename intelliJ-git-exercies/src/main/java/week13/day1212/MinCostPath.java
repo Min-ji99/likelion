c44847638c421a6fca5bfbd119573d38e1cbfbc3
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-12 09:20:23 +0900

    Feat : 첫째칸만 있는 경우 dp배열 채우기

--- intelliJ-git-exercies/src/main/java/week13/day1212/MinCostPath.java
+++ intelliJ-git-exercies/src/main/java/week13/day1212/MinCostPath.java
@@ -5,7 +5,7 @@ public class MinCostPath {
         int[][] matrix=new int[][]{{1, 3, 2}, {4, 6, 2},{1, 2, 4}};
         int[][] dp=new int[matrix.length][matrix[0].length];
 
-
+        dp[0][0]=matrix[0][0];
 
     }
 }
