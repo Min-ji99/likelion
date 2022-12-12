a154c6e29a674504f9596f53199efa377e595d60
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-12 09:19:25 +0900

    Feat : dp배열 생성하기

---
+++ intelliJ-git-exercies/src/main/java/week13/day1212/MinCostPath.java
@@ -0,0 +1,11 @@
+package week13.day1212;
+
+public class MinCostPath {
+    public static void main(String[] args) {
+        int[][] matrix=new int[][]{{1, 3, 2}, {4, 6, 2},{1, 2, 4}};
+        int[][] dp=new int[matrix.length][matrix[0].length];
+
+
+
+    }
+}
