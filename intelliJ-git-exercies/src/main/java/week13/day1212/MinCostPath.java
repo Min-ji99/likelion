d822b103179c151c4538eda74a70a2b0b9f83d52
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-12 09:23:49 +0900

    Feat : 첫번째 행만 있는 경우 dp배열 채우기

--- intelliJ-git-exercies/src/main/java/week13/day1212/MinCostPath.java
+++ intelliJ-git-exercies/src/main/java/week13/day1212/MinCostPath.java
@@ -1,11 +1,18 @@
 package week13.day1212;
 
+import java.util.Arrays;
+
 public class MinCostPath {
     public static void main(String[] args) {
         int[][] matrix=new int[][]{{1, 3, 2}, {4, 6, 2},{1, 2, 4}};
         int[][] dp=new int[matrix.length][matrix[0].length];
 
         dp[0][0]=matrix[0][0];
-
+        for(int i=1; i<matrix[0].length; i++){
+            dp[0][i]=dp[0][i-1]+matrix[0][i];
+        }
+        for(int i=0; i< matrix.length; i++) {
+            System.out.println(Arrays.toString(dp[i]));
+        }
     }
 }
