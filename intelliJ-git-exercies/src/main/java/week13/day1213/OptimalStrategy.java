7241876f52c5a28a7b588dc509f05dffb76e5e34
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-13 09:45:14 +0900

    Feat : 숫자가 하나만 있는 경우

--- intelliJ-git-exercies/src/main/java/week13/day1213/OptimalStrategy.java
+++ intelliJ-git-exercies/src/main/java/week13/day1213/OptimalStrategy.java
@@ -1,16 +1,32 @@
 package week13.day1213;
 
-public class OptimalStrategy {
-    class Pair{
-        int left;
-        int right;
-        public Pair(int left, int right){
-            this.left=left;
-            this.right=right;
-        }
+class Pair{
+    int left;
+    int right;
+    public Pair(int left, int right){
+        this.left=left;
+        this.right=right;
     }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
+public class OptimalStrategy {
     public static void main(String[] args) {
-        int[] arr=new int[]{2, 7, 40, 19, 4, 9};
-        Pair[][] dp=new Pair[4][4];
+        int[] coins=new int[]{2, 7, 40, 19};
+        Pair[][] dp=new Pair[coins.length][coins.length];
+
+        for(int i=0; i<coins.length; i++){
+            dp[i][i]=new Pair(coins[i], 0);
+        }
+
+        for(int i=0; i<coins.length; i++){
+            System.out.println(dp[i][i].toString());
+        }
     }
 }
