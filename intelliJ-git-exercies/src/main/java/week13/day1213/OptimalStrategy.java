3f85619e5b8a7007b2fa49520d57a8a57488b035
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-13 09:29:07 +0900

    Feat : 한칸당 두개의 값을 넣는 dp배열 생성

---
+++ intelliJ-git-exercies/src/main/java/week13/day1213/OptimalStrategy.java
@@ -0,0 +1,16 @@
+package week13.day1213;
+
+public class OptimalStrategy {
+    class Pair{
+        int left;
+        int right;
+        public Pair(int left, int right){
+            this.left=left;
+            this.right=right;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr=new int[]{2, 7, 40, 19, 4, 9};
+        Pair[][] dp=new Pair[4][4];
+    }
+}
