6abdd9e23856573bd753d634e9504268841d32a1
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-12-13 17:20:18 +0900

    Feat : from부터 to까지의 합

--- intelliJ-git-exercies/src/main/java/week13/day1213/OptimalStrategy.java
+++ intelliJ-git-exercies/src/main/java/week13/day1213/OptimalStrategy.java
@@ -17,6 +17,34 @@ class Pair{
     }
 }
 public class OptimalStrategy {
+    public static Pair calcPair(int[] coins, int i, int j){
+        int left=0, right=0;
+        boolean turnFlag=true;
+        while(i<=j){
+            if(coins[i]<coins[j]){
+                if(turnFlag)
+                    left+=coins[j];
+                else right+=coins[j];
+                j-=1;
+            }else{
+                if(turnFlag)
+                    left+=coins[i];
+                else right+=coins[i];
+                i+=1;
+            }
+            turnFlag=!turnFlag;
+        }
+
+        return new Pair(left, right);
+    }
+    public static int sumFromTo(int[] coins, int from, int to){
+        int sum=0;
+        while(from<to){
+            sum+=coins[from];
+            from+=1;
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         int[] coins=new int[]{2, 7, 40, 19};
         Pair[][] dp=new Pair[coins.length][coins.length];
@@ -24,15 +52,16 @@ public class OptimalStrategy {
         for(int i=0; i<coins.length; i++){
             dp[i][i]=new Pair(coins[i], 0);
         }
-        for(int i=0; i<coins.length;i++){
-            int j=i+1;
-            if(j>=coins.length) break;
-            if(dp[i][j-1].left<dp[i+1][j].left){
-                dp[i][j]=new Pair(dp[i+1][j].left, dp[i][j-1].left);
-            }else{
-                dp[i][j]=new Pair(dp[i][j-1].left, dp[i+1][j].left);
+
+        for(int k=1; k<coins.length; k++){
+            for(int i=0; i<coins.length; i++){
+                int j=i+k;
+                if(j>=coins.length) break;
+                dp[i][j]=calcPair(coins, i, j);
+                System.out.println("i : "+i+", j : "+j+" "+dp[i][j].toString());
             }
-            System.out.println(dp[i][j].toString());
         }
+        System.out.println(sumFromTo(coins, 1, 3));
+        //System.out.println(dp[0][coins.length-1].toString());
     }
 }
